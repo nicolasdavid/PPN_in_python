2493f512ffd9003f16cace9006248d2673a65745
Nicolas David <nicolasdavid@users.noreply.github.com>
2017-03-14 15:24:30 +0100

    explain ppl_wrapper with some examples in docstring

--- src/ppl_wrapper.py
+++ src/ppl_wrapper.py
@@ -1,6 +1,18 @@
 import ppl
 
 class Parameter (ppl.Variable):
+    """
+    This class allows to consider sets of Parameter which is not allowed using ppl.Variable
+
+    Example:
+        v = Parameter(0)
+        print(v)
+        print(v.__hash__())
+        expr = ppl.Linear_Expression(v + 2)
+
+        listVars = set()
+        listVars.add(Parameter(0))
+    """
     def __hash__(self):
     # Use only one variable per dimension to avoid conflicts
         return self.id()
